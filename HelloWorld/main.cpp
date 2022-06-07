9a35048ed4d208d9b5cac0bbcba233b59f503281
haibo2.liu <haibo2.liu@ly.com>
2022-06-07 17:45:46 +0800

    update logos

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -4,52 +4,40 @@
 
 using namespace kiwano;
 
-class MyRunner : public Runner
+void Setup()
 {
-public:
-	MyRunner()
-	{
-		// 游戏设置
-		Settings settings;
-		settings.window.title = "Hello World";
-		settings.window.width = 640;
-		settings.window.height = 480;
-		settings.bg_color = Color::Rgb(0xE5E5E5);
+	// 创建舞台
+	StagePtr stage = new Stage;
 
-		SetSettings(settings);
-	}
+	// 创建精灵
+	SpritePtr sprite = new Sprite("../logo/logo_text.png");
 
-	void OnReady() override
-	{
-		// 创建舞台
-		StagePtr stage = new Stage;
+	// 修改精灵位置, 使精灵在屏幕上居中
+	sprite->SetPosition(640 / 2, 480 / 2);
 
-		// 创建精灵
-		SpritePtr sprite = new Sprite("../logo/logo_text.png");
+	// 修改精灵锚点, 使图片中心对齐屏幕中心
+	sprite->SetAnchor(0.5, 0.5);
 
-		// 修改精灵位置, 使精灵在屏幕上居中
-		sprite->SetPosition(640 / 2, 480 / 2);
+	// 修改缩放率, 图片缩小到 0.3 倍
+	sprite->SetScale(0.3f, 0.3f);
 
-		// 修改精灵锚点, 使图片中心对齐屏幕中心
-		sprite->SetAnchor(0.5, 0.5);
+	// 添加到舞台中
+	stage->AddChild(sprite);
 
-		// 修改缩放率, 图片缩小到 0.3 倍
-		sprite->SetScale(0.3f, 0.3f);
-
-		// 添加到舞台中
-		stage->AddChild(sprite);
-
-		// 进入舞台
-		Director::GetInstance().EnterStage(stage);
-	}
-};
+	// 进入舞台
+	Director::GetInstance().EnterStage(stage);
+}
 
 int WINAPI wWinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ PWSTR, _In_ int)
 {
-	// 创建运行器
-	RunnerPtr runner = new MyRunner();
+	// 游戏设置
+	Settings settings;
+	settings.window.title = "Hello World";
+	settings.window.width = 640;
+	settings.window.height = 480;
+	settings.bg_color = Color::Rgb(0xE5E5E5);
 
 	// 运行
-	Application::GetInstance().Run(runner);
+	Application::GetInstance().Run(settings, Setup);
 	return 0;
 }
